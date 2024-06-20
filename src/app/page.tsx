f501a1894a4618de1f0be0880c7db5dc89fdc4f7
Josh Snapp <josh.w.snapp@gmail.com>
2024-06-20 15:44:16 -0400

    Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,12 +24,7 @@ export default function Home() {
       <Hero />
       <Features />
       <Video />
-      <Brands />
-      <AboutSectionOne />
-      <AboutSectionTwo />
-      <Testimonials />
       <Pricing />
-      <Blog />
       <Contact />
     </>
   );
--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -12,10 +12,10 @@ const Contact = () => {
               "
             >
               <h2 className="mb-3 text-2xl font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">
-                Need Help? Open a Ticket
+                Subscribe to our email list
               </h2>
               <p className="mb-12 text-base font-medium text-body-color">
-                Our support team will get back to you ASAP via email.
+                lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               </p>
               <form>
                 <div className="-mx-4 flex flex-wrap">
@@ -67,15 +67,12 @@ const Contact = () => {
                   </div>
                   <div className="w-full px-4">
                     <button className="rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark">
-                      Submit Ticket
+                      Submit Email
                     </button>
                   </div>
                 </div>
               </form>
             </div>
-          </div>
-          <div className="w-full px-4 lg:w-5/12 xl:w-4/12">
-            <NewsLatterBox />
           </div>
         </div>
       </div>
