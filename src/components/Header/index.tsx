bff90c89fbe8689783c0408a4fe452cfb4c7c17b
Josh Snapp <josh.w.snapp@gmail.com>
2024-12-13 15:14:18 -0500

    update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -50,30 +50,6 @@ const Header = () => {
       >
         <div className="container">
           <div className="flex flex-col items-center justify-center bg-white px-4 py-2 dark:bg-dark">
-            <div className="flex items-center pr-5">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="mr-2 h-4 w-4 text-primary"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.948V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                />
-              </svg>
-              <div>
-                <a
-                  href="tel:+3212227176"
-                  className="text-xs text-primary dark:text-primary sm:text-sm md:text-base"
-                >
-                  (321)-222-7176
-                </a>
-              </div>
-            </div>
             <div className="mt-1 flex items-center">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -94,11 +70,43 @@ const Header = () => {
               </svg>
               <div className="flex">
                 <p className="pr-2 text-xs sm:text-sm md:text-base">
-                  M-F 8am-6pm
+                  Mon-Fri 8am-6pm
                 </p>
                 <p className="pr-2 text-xs sm:text-sm md:text-base">
                   Sat 8am-12pm
                 </p>
+              </div>
+            </div>
+            <div className="flex items-center pr-5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="mr-2 h-4 w-4 text-primary"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.948V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                />
+              </svg>
+              <div>
+                <a
+                  href="tel:+3212227176"
+                  className="pr-5 text-xs text-primary dark:text-primary sm:text-sm md:text-base"
+                >
+                  (321)-222-7176
+                </a>
+              </div>
+              <div>
+                <a
+                  href="email"
+                  className="text-xs text-primary dark:text-primary sm:text-sm md:text-base"
+                >
+                  sales@cleanfoldlaundry.com
+                </a>
               </div>
             </div>
           </div>
